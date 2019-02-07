f211856f6a50ee28aa4c6bf84296b6bfe8f6bd22
Facundo Arca <farca9@gmail.com>
2019-02-07 20:30:38 -0300

    Avances en fragments - azu ing

--- app/src/main/java/com/findemes/fragments/TabBalanceFragment.java
+++ app/src/main/java/com/findemes/fragments/TabBalanceFragment.java
@@ -1,6 +1,8 @@
 package com.findemes.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,14 +10,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.findemes.R;
 import com.findemes.model.Movimiento;
 import com.findemes.room.MyDatabase;
 import com.findemes.util.BalanceRecyclerAdapter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TabBalanceFragment extends Fragment{
@@ -27,6 +32,14 @@ public class TabBalanceFragment extends Fragment{
 
     //ROOM
     private MyDatabase database;
+    private ListView listaMovimientos;
+    private TextView mesActual;
+    private TextView ingresos;
+    private TextView gastos ;
+    private RoundCornerProgressBar progressBar;
+
+    private double totalGastos = 0.0;
+    private double totalIngresos = 0.0;
 
     //private List<Movimiento> database;
 
@@ -44,14 +57,14 @@ public class TabBalanceFragment extends Fragment{
         database = MyDatabase.getInstance(getContext());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tab_balance, container, false);
-        ListView listaMovimientos = v.findViewById(R.id.lista_movimientos);
-        TextView mesActual = v.findViewById(R.id.title_label);
-        TextView ingresos = v.findViewById(R.id.monto_total_ingresos);
-        TextView gastos = v.findViewById(R.id.monto_total_gastos);
-
+        mesActual = v.findViewById(R.id.title_label);
+        ingresos = v.findViewById(R.id.monto_total_ingresos);
+        gastos = v.findViewById(R.id.monto_total_gastos);
+        progressBar = v.findViewById(R.id.progressbar);
 
         recyclerView = v.findViewById(R.id.lista_movimientos);
         recyclerView.setHasFixedSize(true);
@@ -70,9 +83,100 @@ public class TabBalanceFragment extends Fragment{
         }).start();
 
 
-
+        totalMovimientos();
+        obtenerMes();
 
         return v;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void totalMovimientos(){
+        //Setea la progress bar y el total de gastos e ingresos
+        final List<Movimiento> movimientos=new ArrayList<>();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                movimientos.addAll(database.getMovimientoDAO().getAll());
+                System.out.println(movimientos.size());
+                for(int i=0; i<movimientos.size(); i++){
+                    if(movimientos.get(i).isGasto()){
+                        //si es gasto
+                        totalGastos+= movimientos.get(i).getMonto();
+                    }
+                    else{
+                        //si es ingreso
+                        totalIngresos += movimientos.get(i).getMonto();
+                    }
+                }
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ingresos.setText(String.valueOf(totalIngresos));
+                        gastos.setText(String.valueOf(totalGastos));
+                        if(totalGastos==0 && totalIngresos==0){
+                            progressBar.setProgress(50);
+                        }
+                        else{
+                            float total = Float.valueOf(String.valueOf((totalIngresos*100)/(totalIngresos+totalGastos)));
+                            System.out.println(total);
+                            progressBar.setProgress(total);
+                        }
+
+                    }
+                });
+            }
+        }).start();
+
+
+    }
+
+
+    private void obtenerMes(){
+        //Setea el mes actual
+
+        int mes = new Date().getMonth();
+
+        switch(mes){
+            case 0:
+                mesActual.setText("ENERO");
+                break;
+            case 1:
+                mesActual.setText("FEBRERO");
+                break;
+            case 2:
+                mesActual.setText("MARZO");
+                break;
+            case 3:
+                mesActual.setText("ABRIL");
+                break;
+            case 4:
+                mesActual.setText("MAYO");
+                break;
+            case 5:
+                mesActual.setText("JUNIO");
+                break;
+            case 6:
+                mesActual.setText("JULIO");
+                break;
+            case 7:
+                mesActual.setText("AGOSTO");
+                break;
+            case 8:
+                mesActual.setText("SEPTIEMBRE");
+                break;
+            case 9:
+                mesActual.setText("OCTUBRE");
+                break;
+            case 10:
+                mesActual.setText("NOVIEMBRE");
+                break;
+            case 11:
+                mesActual.setText("DICIEMBRE");
+                break;
+
+        }
+    }
 }
